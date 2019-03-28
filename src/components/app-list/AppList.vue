dd9ef174a53382c3b1511e6b653eaac4de131f95
Alexandre Ferreira <al_x@tuta.io>
2019-03-28 12:18:42 +0000

    Separate apps by category

--- src/components/app-list/AppList.vue
+++ src/components/app-list/AppList.vue
@@ -1,21 +1,26 @@
 <template>
   <div>
     <input type="text" class="filterInput" v-model="filter" placeholder="Search">
-    <ul class="list">
-      <AppCheck
-        v-for="(app, index) in filteredApps"
-        :name="app.name"
-        :code="app.code"
-        :logoUrl="app.logoUrl"
-        :key="index"
-        @handleAppCheck="(payload) => $emit('handleAppCheck', payload)"
-      />
+    <ul class="categoryList">
+      <li class="category" v-for="(category, index) in categories" :key="index">
+        <h2 class="category__title">{{ category }}</h2>
+        <ul class="appList">
+          <AppCheck
+            v-for="(app, index) in appsOfCategory(category)"
+            :name="app.name"
+            :code="app.code"
+            :logoUrl="app.logoUrl"
+            :key="index"
+            @handleAppCheck="(payload) => $emit('handleAppCheck', payload)"
+          />
+        </ul>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-import appList from "../../shared/data/appList";
+import apps from "../../shared/data/apps";
 import AppCheck from "../app-check/AppCheck.vue";
 
 export default {
@@ -23,16 +28,39 @@ export default {
   components: { AppCheck },
   data() {
     return {
-      apps: appList,
+      apps: apps,
       filter: "",
       selectedApps: []
     };
+  },
+  created() {
+    this.categories = this.getCategories();
   },
   computed: {
     filteredApps() {
       /* Regex will be case insentive */
       const regex = new RegExp(this.filter, "i");
       const filteredApps = this.apps.filter(app => app.name.match(regex));
+
+      return filteredApps;
+    }
+  },
+  methods: {
+    getCategories() {
+      const filteredApps = apps.reduce((acc, app) => {
+        if (!acc) {
+          return [app.category];
+        }
+
+        return acc.find(category => category === app.category)
+          ? acc
+          : [...acc, app.category];
+      }, null);
+
+      return filteredApps;
+    },
+    appsOfCategory(category) {
+      const filteredApps = apps.filter(app => app.category === category);
 
       return filteredApps;
     }
